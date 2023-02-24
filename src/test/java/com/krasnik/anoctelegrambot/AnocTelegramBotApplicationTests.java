a5b7d4da89c6f3797ddb157af9550d48ae3fe676
vlad-1308 <vskrasnik@yandex.ru>
2023-02-24 18:33:57 +0300

    ANOC-0: Create a Spring Boot App

---
+++ src/test/java/com/krasnik/anoctelegrambot/AnocTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krasnik.anoctelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnocTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
