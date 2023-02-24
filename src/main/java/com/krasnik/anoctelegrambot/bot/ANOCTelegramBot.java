86f4eda190089dcb4790a62de25c44eebf4dc649
vlad-1308 <vskrasnik@yandex.ru>
2023-02-24 20:20:11 +0300

    ANOC-1: Add telegram bot to the project

---
+++ src/main/java/com/krasnik/anoctelegrambot/bot/ANOCTelegramBot.java
@@ -0,0 +1,47 @@
+package com.krasnik.anoctelegrambot.bot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class ANOCTelegramBot extends TelegramLongPollingBot {
+
+    @Value("${bot.username}")
+    private String userName;
+
+    @Value("${bot.token}")
+    private String token;
+
+    @Override
+    public String getBotUsername() {
+        return userName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String message  = update.getMessage().getText();
+            String chatId   = update.getMessage().getChatId().toString();
+
+            SendMessage sendMessage = new SendMessage();
+            sendMessage.setChatId(chatId);
+            sendMessage.setText(message);
+
+            try {
+                execute(sendMessage);
+            } catch (TelegramApiException e) {
+                //todo add logging to the project.
+                e.printStackTrace();
+            }
+        }
+    }
+}
